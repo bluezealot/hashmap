9dd282f0631366c2da2571401c5b779e888b13ab
bluezealot <bluezealot@gmail.com>
2022-02-22 10:32:15 +0800

    Add tree implementation.

--- src/main/java/com/bluezealot/hashmap/Bucket.java
+++ src/main/java/com/bluezealot/hashmap/Bucket.java
@@ -2,15 +2,20 @@ package com.bluezealot.hashmap;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
-public class Bucket<K,V> {
+public class Bucket<K extends Comparable,V> {
     private List<KeyValueEntity<K,V>> dataSet = new ArrayList<>();
+    private TreeSet<KeyValueEntity> treeDataSet = new TreeSet();
 
     public void add(KeyValueEntity<K,V> data) {
         dataSet.add(data);
     }
 
     public KeyValueEntity<K,V> getData(K key) {
+        KeyValueEntity<K,V> search = new KeyValueEntity<K,V>();
+        search.setKey(key);
+        // return treeDataSet.ceiling(search);
         return dataSet.stream().filter(data -> 
         key.equals(data.getKey()))
         .findAny()
--- src/main/java/com/bluezealot/hashmap/KeyValueEntity.java
+++ src/main/java/com/bluezealot/hashmap/KeyValueEntity.java
@@ -1,10 +1,27 @@
 package com.bluezealot.hashmap;
 
+import java.util.Comparator;
+
 import lombok.Data;
 
 @Data
-public class KeyValueEntity <k,v>{
+public class KeyValueEntity <k extends Comparable,v> implements Comparable{
     private k key;
     private v value;
 
+    @Override
+    public boolean equals(Object o){
+        KeyValueEntity<k,v> target = (KeyValueEntity<k,v>)o;
+        return this.key.equals(target.key);
+    }
+
+    @Override
+    public int hashCode(){
+        return key.hashCode();
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return key.compareTo(((KeyValueEntity)o).getKey());
+    }
 }
