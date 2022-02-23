2f8d88ac0ac256f3d19369524ac73931b2f8a286
bluezealot <bluezealot@gmail.com>
2022-02-23 17:48:41 +0800

    修正上课时演示失败的问题。

--- src/main/java/com/bluezealot/hashmap/KeyValueEntity.java
+++ src/main/java/com/bluezealot/hashmap/KeyValueEntity.java
@@ -16,11 +16,11 @@ public class KeyValueEntity <k extends Comparable,v> implements Comparable{
 
     @Override
     public int hashCode(){
-        return 0;
+        return key.hashCode();
     }
 
     @Override
     public int compareTo(Object o) {
-        return 0;
+        return key.compareTo(((KeyValueEntity)o).getKey());
     }
 }
